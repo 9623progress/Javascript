131f7e295707a14653531f2eb682c988cb6c3dd4
Pragati Jadhav <jpragati760@gmail.com>
2024-05-28 09:39:09 +0530

    controll flow completed

---
+++ 13.controlFlow.js
@@ -0,0 +1,12 @@
+// if
+if (2 === 2) {
+  console.log("if true");
+} else if (3 === 3) {
+  console.log("else if true");
+} else {
+  console.log("nothing is true;");
+}
+
+while (true) {
+  console.log("while");
+}
