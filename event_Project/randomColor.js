2adf1deab6ca06e4b9ab32dc3d2e7c4031a0d6e1
Pragati Jadhav <jpragati760@gmail.com>
2024-06-02 19:22:18 +0530

    event project done

---
+++ event_Project/randomColor.js
@@ -0,0 +1,31 @@
+const start = document.querySelector(".start");
+const stop = document.querySelector(".stop");
+console.log(start);
+
+let hex = "0123456789ABCDEF";
+
+let color = "#";
+
+const generateRandom = () => {
+  color = "#";
+  for (let i = 0; i < 6; i++) {
+    let random = Math.floor(Math.random() * 16);
+    color = color + hex[random];
+  }
+  document.body.style.backgroundColor = color;
+  //   console.log(color);
+};
+
+let interval;
+
+const StartChangeColor = () => {
+  console.log("startChange call");
+  interval = setInterval(generateRandom, 1000);
+};
+
+const stopChangeColor = () => {
+  clearInterval(interval);
+};
+
+start.addEventListener("click", StartChangeColor);
+stop.addEventListener("click", stopChangeColor);
